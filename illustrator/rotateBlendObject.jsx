8559ebddee819ebee2f26b81b28eb7a492088db9
Changbai Li <lcb931023@gmail.com>
2014-04-08 01:22:00 -0400

    Removed legacy codes.

--- illustrator/rotateBlendObject.jsx
+++ illustrator/rotateBlendObject.jsx
@@ -11,7 +11,7 @@
 
 /*** USER VARIABLES. EDIT THIS PART, YO. ***/
 // 1. The number of times the selection will be copied.
-var copyTime = 10;
+var duplicateTime = 4;
 // 2. The coordinate this selection will be copy-rotated around. Default to anchor point of selection
 var pivotPoint = {
     //x: 100,
@@ -39,109 +39,23 @@ function rotationalBlend(pX, pY)
     }
     // Blend such object
     var dup;
-    for ( i = 0; i < copyTime; i++)
+    for ( i = 0; i < duplicateTime; i++)
     {
         dup = groupedSelection.duplicate();
+        // If a pivot point is provided, rotate around that
         if (pX !== undefined && pY !== undefined)
         {
-            dup.rotate(360/copyTime * (i + 1) );
+            // TODO: Figure out the math for rotation around pivot point. Below are completely wrong maths and they only serve as code examples
+            var pivotMatrix = app.getTranslationMatrix (pX, pY);
+            pivotMatrix = concatenateRotationMatrix(pivotMatrix, 360/duplicateTime * (i + 1) );
+            dup.transform(pivotMatrix);
         } else {
-            dup.rotate(360/copyTime * (i + 1) );
+            // if not, rotate around its own anchor point
+            dup.rotate(360/duplicateTime * (i + 1) );
         }
     }
     // apparently the variable dup is causing illustrator to crash. Hot fix with nullifying it.
     dup = null;
 }
 
-/*
-if (pivotPoint.x !== undefined || pivotPoint.y !== undefined)
-{
-        rotationalBlend(pivotPoint.x, pivotPoint.y);
-    } else {
-        rotationalBlend (  );
-}
-
-function rotationalBlend(pX, pY)
-{
-    
-}
-*/
-
 // TODO: Make it ac tually blend objects, not just copy them
-
-
-
-
-
-
-
-
-
-
-
-/*
-// create an empty array to hold anchor points
-var myPoints = [];
-
-// get all of the pathItems in the document
-var thePaths = myDocument.pathItems;
-
-// process the pathItems (add all anchors to the myPoints array)
-processPathItems(thePaths);
-
-// make a new layer
-var newLayer = myDocument.layers.add();
-
-// add a rectangle for every point in myPoints
-addRects(myPoints);
-
-
-
-// visits every PathItem in a PathItems object
-// visits every PathPoint in those PathItem objects
-// adds the anchor of every PathPoint to the myPoints array
-function processPathItems(pathItems)
-{
-    for(var i = 0; i < pathItems.length; i++)
-    {
-        var pathPoints = pathItems[i].pathPoints;
-        
-        for(var j = 0; j < pathPoints.length; j++)
-        {
-            myPoints.push(pathPoints[j].anchor);
-        }    
-    }
-}
-
-// given an array of points (each point is [x, y] )
-// makes a rectangle at that position
-function addRects(points)
-{
-        for(var i = 0; i < points.length; i++)
-        {
-                makeRect(points[i]);
-        }
-}
-
-// given an anchor point [x, y]
-// create a rectangle centered on that anchor
-function makeRect(anchor)
-{
-    // define the top, left, width, and height of the rectangle
-    var top = anchor[1]+2;
-    var left = anchor[0]-2;
-    var width = 4;
-    var height = 4;
-    
-    // visit newLayer's pathItems array, and add a rectangle object to it
-    var rect = newLayer.pathItems.rectangle(top,left,width,height);
-    // rect is the rectangle we just created
-    
-    // 
-    rect.filled = false;
-    rect.stroked = true;
-    rect.strokeColor = black;
-    rect.strokeWidth = 0.25;
- }
- 
- */
